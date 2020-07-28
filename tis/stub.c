28a962fee2d4f0afab0d6fb03f2ce1b65ef4d50c
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-07-28 13:55:39 +0200

    Special checks for missing files.

--- tis/stub.c
+++ tis/stub.c
@@ -2,3 +2,19 @@
 int rand(void) {
   return 137;
 }
+
+#ifdef __TIS_USER_OPEN
+
+#include <stdarg.h>
+#include <assert.h>
+
+int open(const char * filename, int flags, ...) {
+  va_list va;
+  va_start(va, flags);
+  int rv = __mkfs_open(filename, flags, va);
+  //@ assert UB: rv >= 0;
+  va_end(va);
+  return rv;
+}
+
+#endif // __TIS_USER_OPEN
