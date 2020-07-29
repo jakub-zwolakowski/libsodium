b69ffb25d96c0ece2bde7ac7ffcad903aea9b0f0
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-07-29 16:34:10 +0200

    Comment.

--- tis/stub.c
+++ tis/stub.c
@@ -5,6 +5,12 @@ int rand(void) {
 
 #ifdef __TIS_USER_OPEN
 
+/* All the files in the tests are supposed to open correctly.
+   In order to make sure that my filesystem configuration in the tis.config file
+   is correct I wrap the 'open' function with an ACSL assertion. If there is any
+   problem (i.e. if the return code is not 0), the Analyzer will report an
+   Invalid Property and the test will be shown in red in TIS-CI. */
+
 #include <stdarg.h>
 #include <assert.h>
 
