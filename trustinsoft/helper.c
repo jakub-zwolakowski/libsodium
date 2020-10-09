54e18b431b2e2a5355c7917c0883aea49dceb8aa
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-10-09 10:45:54 +0200

    Stub the sysconf function.

--- trustinsoft/helper.c
+++ trustinsoft/helper.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 size_t __mkfs_preallocate_size = 1000000;
 
@@ -6,12 +7,17 @@ int rand(void) {
   return 137;
 }
 
-int tis_getrandom(void * const buf_, size_t size, int flags)
-{ 
+int tis_getrandom(void * const buf_, size_t size, int flags) {
   unsigned char *buf = (unsigned char*) buf_;
-  
+
   for (size_t i = 0; i < size; i++)
     *buf++ = i;
 
   return (int)size;
+}
+
+/*@ ensures sysconf_OK: \result != -1; */
+long sysconf(int name) {
+  if(name == _SC_PAGESIZE) return 4096;
+  return -1;
 }
